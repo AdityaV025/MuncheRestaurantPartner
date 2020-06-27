8f822cdd73f8511ce82593c808e36b886db8c441
Aditya Verma <aditya.verma5225@gmail.com>
2020-06-27 14:57:01 +0530

    Complete phoneNumber Authentication functionality

--- app/src/main/java/com/example/muncherestaurantpartner/MainActivity.java
+++ app/src/main/java/com/example/muncherestaurantpartner/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.muncherestaurantpartner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import UI.LoginActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseUser mCurrentUser;
+    private FirebaseUser firebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+
+
     }
+
+    private void init() {
+        mAuth = FirebaseAuth.getInstance();
+        firebaseUser = mAuth.getCurrentUser();
+        mCurrentUser = mAuth.getCurrentUser();
+    }
+
+    private void sendUserToLogin() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mCurrentUser == null) {
+            sendUserToLogin();
+        }
+    }
+
 }
