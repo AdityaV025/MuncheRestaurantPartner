61b65a68ce093d73120b6db7f8aa7d249d62c7ef
Aditya Verma <aditya.verma5225@gmail.com>
2020-06-27 14:39:43 +0530

    Initial commit

---
+++ app/src/main/java/UI/LoginActivity.java
@@ -0,0 +1,16 @@
+package UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.muncherestaurantpartner.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
