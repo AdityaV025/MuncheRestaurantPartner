2cacc756afd9062c2581c677ac801a4aaae24033
Aditya Verma <aditya.verma5225@gmail.com>
2020-07-19 23:38:50 +0530

    progress in CreateRestaurantActivity

--- app/src/main/java/UI/CreateRestaurantActivity.java
+++ app/src/main/java/UI/CreateRestaurantActivity.java
@@ -1,8 +1,11 @@
 package UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.os.Bundle;
@@ -16,22 +19,29 @@ import android.widget.Toast;
 import com.example.muncherestaurantpartner.MainActivity;
 import com.example.muncherestaurantpartner.R;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.GeoPoint;
 
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+
+import Utils.GPSTracker;
 
 public class CreateRestaurantActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText mRestaurantName,mRestaurantPhoneNumber, mRestaurantCity, mRestaurantAddress, mRestaurantAveragePrice;
     private Button mSaveResInfoBtn;
     private RadioButton mOwnerRadioBtn, mManagerRadioBtn;
-    private String resName,resNum,resCity,resAddress,checkedDesignation,RestaurantUid,Token,UserPhoneNumber,restaurantAveragePrice;
-    private Double mLatitude, mLongitude;
-    private List<Address> address;
-    private Geocoder geocoder;
+    private String resName,resNum,resCity,resAddress,checkedDesignation,RestaurantUid,Token,
+            UserPhoneNumber,restaurantAveragePrice,city,state,country,postalCode,knownName,
+            subLocality,subAdminArea,finalAddress,address;
+    private Double latitude, longitude;
+    private List<Address> addresses;
     private FirebaseFirestore db;
+    private GPSTracker gpsTracker;
+    private Geocoder geocoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +49,8 @@ public class CreateRestaurantActivity extends AppCompatActivity implements View.
         setContentView(R.layout.activity_create_restaurant);
 
         init();
+        checkPermission();
+        getLocation();
         mSaveResInfoBtn.setOnClickListener(this);
 
     }
@@ -56,7 +68,6 @@ public class CreateRestaurantActivity extends AppCompatActivity implements View.
         RestaurantUid = getIntent().getStringExtra("RUID");
         UserPhoneNumber = getIntent().getStringExtra("PHONENUMBER");
         Token = getIntent().getStringExtra("TOKEN");
-        geocoder = new Geocoder(this);
     }
 
     @Override
@@ -69,26 +80,13 @@ public class CreateRestaurantActivity extends AppCompatActivity implements View.
                 || mRestaurantAveragePrice.getText().toString().isEmpty()){
 
             Toast.makeText(this, "Please Enter Correct Information", Toast.LENGTH_LONG).show();
+
         }else {
             resName = mRestaurantName.getText().toString();
             resCity  = mRestaurantCity.getText().toString();
             resNum =  mRestaurantPhoneNumber.getText().toString();
             resAddress = mRestaurantAddress.getText().toString();
             restaurantAveragePrice = mRestaurantAveragePrice.getText().toString();
-
-            try {
-                address = geocoder.getFromLocationName(resAddress,5);
-                if (address == null) {
-                    Log.d("NULL_ADDRESS", "Address is NUll");
-                }
-
-                Address location = address.get(0);
-                mLatitude = location.getLatitude();
-                mLongitude = location.getLongitude();
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
 
             if (mOwnerRadioBtn.isChecked()){
                 checkedDesignation = mOwnerRadioBtn.getText().toString();
@@ -106,8 +104,8 @@ public class CreateRestaurantActivity extends AppCompatActivity implements View.
             RestaurantData.put("user_phonenumber", UserPhoneNumber);
             RestaurantData.put("restaurant_uid", RestaurantUid);
             RestaurantData.put("device_token", Token);
-            RestaurantData.put("latitude", mLatitude);
-            RestaurantData.put("longitude", mLongitude);
+            RestaurantData.put("latitude", latitude);
+            RestaurantData.put("longitude", longitude);
             RestaurantData.put("user_role", checkedDesignation);
             RestaurantData.put("average_price", restaurantAveragePrice);
             RestaurantData.put("restaurant_spotimage", "empty");
@@ -123,6 +121,48 @@ public class CreateRestaurantActivity extends AppCompatActivity implements View.
 
         }
 
+    }
+
+    private void checkPermission() {
+        try {
+            if (ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ) {
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 101);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void getLocation() {
+        gpsTracker = new GPSTracker(CreateRestaurantActivity.this);
+        if(gpsTracker.canGetLocation()){
+            latitude = gpsTracker.getLatitude();
+            longitude = gpsTracker.getLongitude();
+
+            geocoder = new Geocoder(CreateRestaurantActivity.this, Locale.getDefault());
+
+            try {
+                addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+            city = addresses.get(0).getLocality();
+            state = addresses.get(0).getAdminArea();
+            country = addresses.get(0).getCountryName();
+            postalCode = addresses.get(0).getPostalCode();
+            knownName = addresses.get(0).getFeatureName();
+            subLocality = addresses.get(0).getSubLocality();
+            subAdminArea = addresses.get(0).getSubAdminArea();
+
+            finalAddress = knownName + ", " + subLocality +  ", " + city + ", " + postalCode;
+
+            mRestaurantAddress.setText(finalAddress);
+
+        }else{
+            gpsTracker.showSettingsAlert();
+        }
     }
 
     private void sendUserToMain() {
